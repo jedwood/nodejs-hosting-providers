9135d136c3c014a65535beb7051e2d44554b58f8
Jed Wood <jed@limechile.com>
2013-05-15 16:59:30 -0500

    super simple app for testing

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    config   = require('nconf'),
+    app     = express();
+
+config.argv().env().file({ file: '../config.json' });
+
+config.defaults({'PORT': 5000, secret: 'default secret.'});
+// //
+// // Set a few variables on `nconf`.
+// //
+// config.set('database:host', '127.0.0.1');
+// config.set('database:port', 5984);
+
+app.configure(function(){
+  app.set('port', config.get('PORT'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.logger('dev'));
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res, next) {
+  res.send({secret: config.get('secret')});
+});
+
+app.listen(app.get('port'), function(){
+  console.log("Node.js Hosting Test listening on port " + config.get('PORT') + ', running in ' + app.settings.env + " mode.");
+});
