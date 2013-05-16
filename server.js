2f55e16d0ba9130708a0bb0913b9e68e1582fead
Jed Wood <jed@limechile.com>
2013-05-16 13:05:01 -0500

    downgrade node version for Azure

--- server.js
+++ server.js
@@ -5,20 +5,13 @@ var express = require('express'),
 config.argv().env().file({ file: '../config.json' });
 
 config.defaults({'PORT': 1337, SECRET: 'default secret.'});
-// //
-// // Set a few variables on `nconf`.
-// //
-// config.set('database:host', '127.0.0.1');
-// config.set('database:port', 5984);
+
 
 app.configure(function(){
   app.set('port', config.get('PORT'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-});
-
-app.configure('development', function(){
   app.use(express.logger('dev'));
   app.use(express.errorHandler());
 });
@@ -28,5 +21,5 @@ app.get('/', function(req, res, next) {
 });
 
 app.listen(app.get('port'), function(){
-  console.log("Node.js Hosting Test listening on port " + config.get('PORT') + ', running in ' + app.settings.env + " mode.");
+  console.log("Node.js Hosting Test listening on port " + config.get('PORT') + ', running in ' + app.settings.env + " mode, Node version is: " + process.version);
 });
